08d61588bfaa072198a5e7a755329f291ee5c24b
Kingsley Victor <javaprodigy56@gmail.com>
2021-12-12 10:03:38 +0100

    set default background color to transparent

--- src/components/TouchableOpacity/index.js
+++ src/components/TouchableOpacity/index.js
@@ -2,7 +2,7 @@ import styled from "styled-components/native";
 
 const TouchableOpacity = styled.TouchableOpacity`
   margin-vertical: ${props => props.marginVertical || "12px"};
-  background-color: ${props => props.backgroundColor || "#fff"};
+  background-color: ${props => props.backgroundColor || "transparent"};
   width: ${props => props.width || "327px"};
   height: ${props => props.height || "78px"};
 `;
