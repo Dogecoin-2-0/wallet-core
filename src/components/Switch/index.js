d19746fa92f41d8e2039dcab57e5599dd3ac5845
Kingsley Victor <javaprodigy56@gmail.com>
2021-12-12 14:06:37 +0100

    update toggle switch component

--- src/components/Switch/index.js
+++ src/components/Switch/index.js
@@ -1,20 +1,35 @@
-import React, { useState } from "react";
-import { Switch } from "react-native";
+import React from "react";
+import propTypes from "prop-types";
+import styled from "styled-components/native";
 
-const ToggleSwitch = () => {
+const Switch = styled.Switch`
+  background-color: ${props => props.backgroundColor || "#cccccc"};
+`;
 
-    const [isToggled, setIsToggled] = useState(false);
-    const toggle = () => setIsToggled(previousState => !previousState);
+const ToggleSwitch = ({ value, onValueChange, trackColor, iosBackgroundColor, thumbColor }) => (
+  <Switch
+    trackColor={trackColor}
+    thumbColor={thumbColor}
+    ios_backgroundColor={iosBackgroundColor}
+    onValueChange={onValueChange}
+    value={value}
+  />
+);
 
-    return (
-        <Switch
-            trackColor={{ true: "#DBA134", false: "#CCCCCC" }}
-            thumbColor="#FFFFFF"
-            ios_backgroundColor="#CCCCCC"
-            onValueChange={toggle}
-            value={isToggled}
-        />
-    );
+ToggleSwitch.defaultPropTypes = {
+  value: false,
+  onValueChange: val => console.log(val),
+  trackColor: { true: "#dba134", false: "#cccccc" },
+  iosBackgroundColor: "#cccccc",
+  thumbColor: "#ffffff"
+};
+
+ToggleSwitch.propTypes = {
+  value: propTypes.bool,
+  onValueChange: propTypes.func,
+  trackColor: propTypes.object,
+  iosBackgroundColor: propTypes.string,
+  thumbColor: propTypes.string
 };
 
 export default ToggleSwitch;
