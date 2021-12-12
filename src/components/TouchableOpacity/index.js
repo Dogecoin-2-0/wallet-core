696dbfc053e601a04359862c9f59352010166477
Kingsley Victor <javaprodigy56@gmail.com>
2021-12-12 09:47:15 +0100

    set default values for highlighted props

--- src/components/TouchableOpacity/index.js
+++ src/components/TouchableOpacity/index.js
@@ -1,10 +1,10 @@
 import styled from "styled-components/native";
 
 const TouchableOpacity = styled.TouchableOpacity`
-  margin-vertical: ${props => props.marginVertical};
-  background-color: ${props => props.backgroundColor};
-  width: ${props => props.width};
-  height: ${props => props.height};
+  margin-vertical: ${props => props.marginVertical || "12px"};
+  background-color: ${props => props.backgroundColor || "#fff"};
+  width: ${props => props.width || "327px"};
+  height: ${props => props.height || "78px"};
 `;
 
 export default TouchableOpacity;
