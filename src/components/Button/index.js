169211fa4d521494ecfc79366138496b046469d4
ghidyon <gideonakunana@gmail.com>
2021-12-12 08:59:07 +0100

    feat: created ToggleSwitch component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,15 +2,10 @@ import styled from "styled-components";
 
 const Button = styled.button`
     border-radius: ${props => props.borderRadius},
-    border: 2px solid ${props => props.theme.main},
-    background: ${props => props.theme.main},
-    color: ${props => props.theme.main}
+    border: 2px solid ${props => props.color},
+    background: ${props => props.backgroundColor},
+    color: ${props => props.color},
+    margin-vertical: ${props => props.marginVertical};
 `;
-
-Button.defaultProps = {
-    theme: {
-        main: "#DBA134"
-    }
-};
 
 export default Button;
