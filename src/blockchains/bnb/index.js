c61d6da80cb409d158ecc96f0c25c540aa5109d4
Kingsley Victor <javaprodigy56@gmail.com>
2021-12-23 15:56:06 +0100

    add function to generate wallet from private key

--- src/blockchains/bnb/index.js
+++ src/blockchains/bnb/index.js
@@ -1,12 +1,18 @@
 import { Wallet } from "ethers";
 import Web3 from "web3";
-import { generateFromMnemonic } from "../../utils";
 import * as constants from "../../constants";
 
+const web3 = new Web3();
+
 export default class SmartChain {
-  static generateWallet(mnemonic = null) {
-    const walletFromMnemonic = Wallet.fromMnemonic(generateFromMnemonic(mnemonic));
+  static generateWalletFromMnemonic(mnemonic) {
+    const walletFromMnemonic = Wallet.fromMnemonic(mnemonic);
     return { address: walletFromMnemonic.address, pk: walletFromMnemonic.privateKey };
+  }
+
+  static generateWalletFromPrivateKey(pk) {
+    const accountFromPrivateKey = web3.eth.accounts.privateKeyToAccount(pk);
+    return { address: accountFromPrivateKey.address, pk: accountFromPrivateKey.privateKey };
   }
 
   static async fetchInfo() {
