e69293c3fefd90c355c9fdf7c6ec55da5388ccd0
theafolayan <theafolayan@gmail.com>
2021-12-23 17:17:01 +0100

    added padding to form area

--- src/screens/onboarding/CreateWallet.js
+++ src/screens/onboarding/CreateWallet.js
@@ -6,7 +6,7 @@ import Screen from "../../components/Screen";
 import AppText from "../../components/AppText";
 
 export default function CreateWallet() {
-  const step = 3;
+  const step = 1;
   return (
     <Screen>
       <View style={styles.row}>
@@ -14,8 +14,10 @@ export default function CreateWallet() {
         <ProgressBar step={step} />
         <AppText style={{ textAlign: "center" }}> {step} / 3</AppText>
       </View>
-      <AppText bold> Create Password</AppText>
-      <AppText grey> This password will unlock your Air wallet only on this service.</AppText>
+      <View style={styles.formArea}>
+        <AppText bold> Create Password</AppText>
+        <AppText grey> This password will unlock your Air wallet only on this service.</AppText>
+      </View>
     </Screen>
   );
 }
@@ -25,5 +27,8 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center"
+  },
+  formArea: {
+    marginTop: 40
   }
 });
