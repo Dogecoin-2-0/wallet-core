3edcbd23cfe51a28d13b22977c20d296f744d9a2
theafolayan <theafolayan@gmail.com>
2022-02-08 19:23:16 +0100

    created reusable bottomsheet component

---
+++ src/components/home/AccountSwitcher.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+export default function AccountSwitcher() {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
--- src/screens/app/Home.js
+++ src/screens/app/Home.js
@@ -1,5 +1,5 @@
 import { Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import React from 'react';
+import React, { useRef } from 'react';
 import Screen from '../../components/Screen';
 import AppText from '../../components/AppText';
 import Icon from '../../components/Icon';
@@ -7,40 +7,62 @@ import colors from '../../constants/colors';
 import Actions from '../../components/home/Actions';
 import TokenCollectiblesSwap from '../../components/home/TokenCollectiblesSwap';
 import TokenCard from '../../components/home/TokenCard';
+import { PortalProvider } from '@gorhom/portal';
+import ReusableBottomSheet from '../../components/extras/ReusableBottomSheet';
 
 export default function Home() {
+  const modalRef = useRef(null);
+
+  const onOpen = () => {
+    modalRef.current?.open();
+  };
+
+  const onClose = () => {
+    modalRef.current?.close();
+  };
   return (
-    <Screen>
-      <View style={styles.row}>
-        <View style={styles.username}>
-          <TouchableOpacity style={styles.rowArea}>
-            <Image source={require('../../../assets/avatar.png')} style={styles.avatar} />
-            <AppText bold yellow>
-              Queen Bee
+    <PortalProvider>
+      <ReusableBottomSheet
+        title="Account"
+        modalRef={modalRef}
+        children={
+          <View>
+            <AppText> My love</AppText>
+          </View>
+        }
+      />
+      <Screen>
+        <View style={styles.row}>
+          <View style={styles.username}>
+            <TouchableOpacity style={styles.rowArea} onPress={onOpen}>
+              <Image source={require('../../../assets/avatar.png')} style={styles.avatar} />
+              <AppText bold yellow>
+                Queen Bee
+              </AppText>
+              <Icon name="chevron-down" />
+            </TouchableOpacity>
+          </View>
+          <Image source={require('../../../assets/dogeroundedLogo.png')} style={styles.logo} />
+        </View>
+        <AppText big>4.8729 ETH</AppText>
+
+        <View style={styles.usd}>
+          <AppText grey>$ 8,391.14</AppText>
+          <View style={[styles.rowArea, { marginHorizontal: 25 }]}>
+            <Icon name="arrow-top-right" color={colors.green} size={20} />
+            <AppText grey green>
+              {'9.97'}%
             </AppText>
-            <Icon name="chevron-down" />
-          </TouchableOpacity>
+          </View>
         </View>
-        <Image source={require('../../../assets/dogeroundedLogo.png')} style={styles.logo} />
-      </View>
-      <AppText big>4.8729 ETH</AppText>
 
-      <View style={styles.usd}>
-        <AppText grey>$ 8,391.14</AppText>
-        <View style={[styles.rowArea, { marginHorizontal: 25 }]}>
-          <Icon name="arrow-top-right" color={colors.green} size={20} />
-          <AppText grey green>
-            {'9.97'}%
-          </AppText>
-        </View>
-      </View>
-
-      <Actions />
-      <TokenCollectiblesSwap />
-      <TokenCard />
-      <TokenCard />
-      <TokenCard />
-    </Screen>
+        <Actions />
+        <TokenCollectiblesSwap />
+        <TokenCard />
+        <TokenCard />
+        <TokenCard />
+      </Screen>
+    </PortalProvider>
   );
 }
 
