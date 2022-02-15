4c4b98ef025b57752a9d734aac353bc8f6649b6c
theafolayan <theafolayan@gmail.com>
2022-02-15 22:51:11 +0100

    implemented accountcard component

--- src/screens/app/SendToken.js
+++ src/screens/app/SendToken.js
@@ -1,18 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
 import React from 'react';
 import AppText from '../../components/AppText';
 import Screen from '../../components/Screen';
 import RecentTransactionCard from '../../components/wallet/RecentTransactionCard';
+import AccountCard from '../../components/wallet/AccountCard';
 
 export default function SendToken() {
   return (
     <Screen>
+      <AppText bold padded>
+        From
+      </AppText>
+      <AccountCard />
+      <AppText bold padded>
+        To
+      </AppText>
       <View>
-        <AppText centered blue bold underlined>
-          Transfer Between by Accounts
+        <AppText centered blue bold underlined padded>
+          Transfer Between My Accounts
         </AppText>
 
-        <AppText bold medium>
+        <AppText bold medium padded>
           Recent
         </AppText>
 
