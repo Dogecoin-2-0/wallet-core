72fcab3a5ca1f23e0fb4c9cce2931c2f0ecf2835
theafolayan <theafolayan@gmail.com>
2022-02-15 17:32:37 +0100

    installed expo barcode scanner

---
+++ src/screens/app/SendToken.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+export default function SendToken() {
+  return (
+    <View>
+      <Text>SendToken</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
