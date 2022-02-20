f1d0fb80c16d9f96971e31f8864a4170833455bc
Kingsley Victor <javaprodigy56@gmail.com>
2022-02-20 23:05:37 +0100

    split send token component

--- src/components/wallet/RecieveAsset.js
+++ src/components/wallet/RecieveAsset.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Share } from 'react-native';
 import * as Clipboard from 'expo-clipboard';
 import React, { useState, useEffect } from 'react';
 import SvgQRCode from 'react-native-qrcode-svg';
@@ -18,6 +18,11 @@ export default function RecieveAsset({ qrValue = '0x', address = '0x' }) {
   const closeAlert = () => {
     setAlertDisplayed(false);
     setAlertMessage('');
+  };
+  const share = () => {
+    Share.share({ message: address.toLowerCase() }).then(share => {
+      console.log('Share result: ', { ...share });
+    });
   };
 
   useEffect(() => {
@@ -45,7 +50,7 @@ export default function RecieveAsset({ qrValue = '0x', address = '0x' }) {
           icon="content-copy"
           onPress={copyAddress}
         />
-        <AppButton title="Share" outlined half icon="share-variant" />
+        <AppButton title="Share" outlined half icon="share-variant" onPress={share} />
         <ReusableAlert close={closeAlert} isSuccessful={true} message={alertMessage} visible={alertDisplayed} />
       </View>
     </>
--- src/screens/app/SendToken.js
+++ src/screens/app/SendToken.js
@@ -1,7 +1,7 @@
-import { StyleSheet, View, TouchableOpacity } from 'react-native';
-import React, { useRef } from 'react';
+/* eslint-disable react/no-children-prop */
+import { StyleSheet, View } from 'react-native';
+import React, { useRef, useState } from 'react';
 import AppText from '../../components/AppText';
-import Screen from '../../components/Screen';
 import RecentTransactionCard from '../../components/wallet/RecentTransactionCard';
 import AccountCard from '../../components/wallet/AccountCard';
 import { PortalProvider } from '@gorhom/portal';
@@ -10,6 +10,46 @@ import AccountSwitcher from '../../components/home/AccountSwitcher';
 import colors from '../../constants/colors';
 import { TextInput } from 'react-native-gesture-handler';
 import { Icon } from '../../components';
+import AppButton from '../../components/AppButton';
+
+function TransferComponent({ setRecipient, onOpen, recipient }) {
+  return (
+    <>
+      <AccountCard onPress={onOpen} />
+      <View style={styles.inputArea}>
+        <AppText bold padded>
+          To
+        </AppText>
+
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            onChangeText={setRecipient}
+            placeholder="Search, public address(0x), or ENS"
+            value={recipient}
+          />
+          <Icon name={recipient.trim().length > 0 ? 'close' : 'qrcode-scan'} />
+        </View>
+
+        {recipient.trim().length === 0 && (
+          <AppText centered blue bold underlined padded>
+            Transfer Between My Accounts
+          </AppText>
+        )}
+      </View>
+
+      {recipient.trim().length === 0 && (
+        <AppText bold medium padded>
+          Recent
+        </AppText>
+      )}
+
+      {recipient.trim().length > 0 && <AppButton title="Next" />}
+
+      {recipient.trim().length === 0 && [1, 2, 3].map(i => <RecentTransactionCard key={i} />)}
+    </>
+  );
+}
 
 export default function SendToken() {
   const accountSwitcherRef = useRef(null);
@@ -20,33 +60,14 @@ export default function SendToken() {
   const onClose = () => {
     accountSwitcherRef.current?.close();
   };
+
+  const [recipient, setRecipient] = useState('');
+
   return (
     <PortalProvider>
       <ReusableBottomSheet title="Account" height={500} modalRef={accountSwitcherRef} children={<AccountSwitcher />} />
 
-      <AccountCard onPress={onOpen} />
-      <View style={styles.inputArea}>
-        <AppText bold padded>
-          To
-        </AppText>
-
-        <View style={styles.inputContainer}>
-          <TextInput style={styles.input} placeholder="Search, public address(0x), or ENS" />
-          <Icon name="qrcode-scan" />
-        </View>
-
-        <AppText centered blue bold underlined padded>
-          Transfer Between My Accounts
-        </AppText>
-      </View>
-
-      <AppText bold medium padded>
-        Recent
-      </AppText>
-
-      {[1, 2, 3].map(i => (
-        <RecentTransactionCard key={i} />
-      ))}
+      <TransferComponent onOpen={onOpen} setRecipient={setRecipient} recipient={recipient} />
       {/* </Screen> */}
     </PortalProvider>
   );
