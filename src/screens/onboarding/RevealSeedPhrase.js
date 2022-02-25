4804faa2a22b5fa56cf5fe03c6711aa87bd28e6e
theafolayan <theafolayan@gmail.com>
2022-02-25 10:07:35 +0100

    added seedphrase blue toggle to reveal seed phrase screen

--- src/screens/onboarding/RevealSeedPhrase.js
+++ src/screens/onboarding/RevealSeedPhrase.js
@@ -1,6 +1,7 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { Image, View, StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import AppButton from '../../components/AppButton';
 import AppText from '../../components/AppText';
 import SeedPhraseWraper from '../../components/onboarding/SeedPhraseWraper';
@@ -8,6 +9,11 @@ import OnboardingProgress from '../../components/OnboardingProgress';
 import Screen from '../../components/Screen';
 
 export default function RevealSeedPhrase({ navigation }) {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
   return (
     <Screen>
       <View style={{ flex: 1 }}>
@@ -23,7 +29,13 @@ export default function RevealSeedPhrase({ navigation }) {
       </View>
 
       <View style={{ flex: 2 }}>
-        <SeedPhraseWraper isInit={true} />
+        {visible ? (
+          <SeedPhraseWraper isInit={true} />
+        ) : (
+          <TouchableOpacity onPress={toggleVisibility}>
+            <Image source={require('../../../assets/seedphrase.png')} style={styles.image} />
+          </TouchableOpacity>
+        )}
       </View>
       <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'flex-end', marginVertical: 10 }}>
         <AppButton title="Continue" onPress={() => navigation.navigate('confirmSeedPhrase')} />
@@ -31,3 +43,9 @@ export default function RevealSeedPhrase({ navigation }) {
     </Screen>
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    alignSelf: 'center'
+  }
+});
