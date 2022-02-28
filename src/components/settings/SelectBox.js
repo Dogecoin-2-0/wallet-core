e3d007fa6ef6e6b948b7c2c90070e731b95283d4
theafolayan <theafolayan@gmail.com>
2022-02-28 16:18:17 +0100

    wrote select box component

---
+++ src/components/settings/SelectBox.js
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React from 'react';
+import AppText from '../AppText';
+import { Icon } from '..';
+// import { TouchableOpacity } from '..';
+
+export default function SelectBox({ title, selectedValue, onPress }) {
+  return (
+    <TouchableOpacity style={styles.container}>
+      <View>
+        <AppText small grey>
+          {title}
+        </AppText>
+        <AppText bold>{selectedValue}</AppText>
+      </View>
+      <View>
+        <Icon name="chevron-down" />
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    marginVertical: 10
+  }
+});
--- src/screens/settings/GeneralSettings.js
+++ src/screens/settings/GeneralSettings.js
@@ -1,27 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 import AppText from '../../components/AppText';
 import Screen from '../../components/Screen';
 import TokenDetailHeader from '../../components/wallet/TokenDetailHeader';
+import SelectBox from '../../components/settings/SelectBox';
 // import App from '../../../App';
 
 export default function GeneralSettings({ navigation }) {
   return (
     <Screen>
       <TokenDetailHeader name="General" goBack={() => navigation.goBack()} />
-
       <AppText padded>Privacy Currency</AppText>
       <AppText grey>
         Select Native to prioritize displaying values in the native currency of the chain (e.g. ETH). Select Fiat to
         prioritize displaying values in your selected fiat currency
       </AppText>
-
       <AppText padded>Current Language</AppText>
       <AppText grey>Translate the application to a different supported language</AppText>
-
       <AppText padded> Search Engine</AppText>
       <AppText grey> Change the default search engine used when entering search terms in the URL bar </AppText>
-
+      <SelectBox title="Base Currency" selectedValue="USD - United States Dollar" />
       <AppText> Account Identicon</AppText>
       <AppText grey> You can customize your account</AppText>
     </Screen>
