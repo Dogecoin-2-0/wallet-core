db603fc17c87511fe92b350ce8addfbabfc599fb
theafolayan <theafolayan@gmail.com>
2022-02-28 17:00:34 +0100

    removed unused imports from general settings screen

--- src/screens/settings/GeneralSettings.js
+++ src/screens/settings/GeneralSettings.js
@@ -1,34 +1,37 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import React from 'react';
 import AppText from '../../components/AppText';
 import Screen from '../../components/Screen';
 import TokenDetailHeader from '../../components/wallet/TokenDetailHeader';
 import SelectBox from '../../components/settings/SelectBox';
 import PrivacyCurrencySwitcher from '../../components/settings/PrivacyCurrencySwitcher';
+import { PortalProvider } from '@gorhom/portal';
 // import App from '../../../App';
 
 export default function GeneralSettings({ navigation }) {
   return (
-    <Screen>
-      <TokenDetailHeader name="General" goBack={() => navigation.goBack()} />
+    <PortalProvider>
+      <Screen>
+        <TokenDetailHeader name="General" goBack={() => navigation.goBack()} />
 
-      <SelectBox title="Base Currency" selectedValue="USD - United States Dollar" />
-      <AppText padded>Privacy Currency</AppText>
-      <AppText grey>
-        Select Native to prioritize displaying values in the native currency of the chain (e.g. ETH). Select Fiat to
-        prioritize displaying values in your selected fiat currency
-      </AppText>
+        <SelectBox title="Base Currency" selectedValue="USD - United States Dollar" />
+        <AppText padded>Privacy Currency</AppText>
+        <AppText grey>
+          Select Native to prioritize displaying values in the native currency of the chain (e.g. ETH). Select Fiat to
+          prioritize displaying values in your selected fiat currency
+        </AppText>
 
-      <PrivacyCurrencySwitcher />
-      <AppText padded>Current Language</AppText>
-      <AppText grey>Translate the application to a different supported language</AppText>
-      <SelectBox title="Language" selectedValue="English" />
-      <AppText padded> Search Engine</AppText>
-      <AppText grey> Change the default search engine used when entering search terms in the URL bar </AppText>
-      <SelectBox title="Engine" selectedValue="Duck Duck Go" />
-      <AppText> Account Identicon</AppText>
-      <AppText grey> You can customize your account</AppText>
-    </Screen>
+        <PrivacyCurrencySwitcher />
+        <AppText padded>Current Language</AppText>
+        <AppText grey>Translate the application to a different supported language</AppText>
+        <SelectBox title="Language" selectedValue="English" />
+        <AppText padded> Search Engine</AppText>
+        <AppText grey> Change the default search engine used when entering search terms in the URL bar </AppText>
+        <SelectBox title="Engine" selectedValue="Duck Duck Go" />
+        <AppText> Account Identicon</AppText>
+        <AppText grey> You can customize your account</AppText>
+      </Screen>
+    </PortalProvider>
   );
 }
 
