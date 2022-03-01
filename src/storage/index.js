3753f6943e6a654124918a03d53d4ca4ee0e7697
Kingsley Victor <javaprodigy56@gmail.com>
2022-03-01 20:24:55 +0100

    begin account impl

---
+++ src/storage/index.js
@@ -0,0 +1,49 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { v4 as uuid } from 'uuid';
+
+export const _saveAccount = (pw, name, address, seedPhrase) => {
+  const id = uuid();
+  return new Promise((resolve, reject) => {
+    AsyncStorage.getItem('accounts').then(accounts => {
+      if (!accounts || accounts === null || typeof accounts === 'undefined')
+        AsyncStorage.setItem('accounts', JSON.stringify([{ id, pw, name, address, seedPhrase }]))
+          .then(() => resolve())
+          .catch(reject);
+      else
+        AsyncStorage.setItem(
+          'accounts',
+          JSON.stringify([...JSON.parse(accounts), { id, pw, name, address, seedPhrase }])
+        )
+          .then(() => resolve())
+          .catch(reject);
+    });
+  });
+};
+
+export const _getAccountById = id => {
+  return new Promise((resolve, reject) => {
+    AsyncStorage.getItem('accounts')
+      .then(accounts => {
+        if (!accounts || accounts === null || typeof accounts === 'undefined')
+          reject(new Error('No account exists on this device'));
+        else {
+          const account = [...JSON.parse(accounts)].find(acc => acc.id === id);
+          if (!account) reject(new Error('Account with specified ID not found'));
+          resolve(account);
+        }
+      })
+      .catch(reject);
+  });
+};
+
+export const getAllAccounts = () => {
+  return new Promise((resolve, reject) => {
+    AsyncStorage.getItem('accounts')
+      .then(accounts => {
+        if (!accounts || accounts === null || typeof accounts === 'undefined')
+          reject(new Error('No account exists on this device'));
+        resolve([...JSON.parse(accounts)]);
+      })
+      .catch(reject);
+  });
+};
